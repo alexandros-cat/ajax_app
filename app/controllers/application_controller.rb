ed470472198d570de15670fcc0455384b0c612cc
alexandros-cat <alexandros.cat@gmail.com>
2020-08-20 18:17:28 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   def index
    @post = post.find(1)
   end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'BASIC_AUTH_USER' && password == 'BASIC_AUTH_PASSWORD'
+    end
+  end  
 
 end
